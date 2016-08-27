37d267cb7491a662805531ec805c147026715906
Tamás András Horváth <htomy92@gmail.com>
2016-08-27 07:32:57 +0200

    Add Preversion 9.2.0

---
+++ spec/palladium-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PalladiumView from '../lib/palladium-view';
+
+describe('PalladiumView', () => {
+    it('has one valid test', () => {
+        expect('life').toBe('easy');
+    });
+});
