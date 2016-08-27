b1beeea1c95304d70cd9094c27ff5eb8740f021d
Tamás András Horváth <htomy92@gmail.com>
2016-08-27 06:59:34 +0200

    Revert "Add 0.9.2 release"
    This reverts commit 89df195ab762d9e68bd0bb52b3258d7e67f68478.

--- spec/palladium-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PalladiumView from '../lib/palladium-view';
-
-describe('PalladiumView', () => {
-    it('has one valid test', () => {
-        expect('life').toBe('easy');
-    });
-});
